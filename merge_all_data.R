c16b23a528caffa1d2949f61de4ddddde20b0e7f
Jackie Ouzman <jackie.ouzman@csiro.au>
2019-05-15 16:08:11 +0930

    test

--- merge_all_data.R
+++ merge_all_data.R
@@ -20,3 +20,5 @@ delegate_wither_villia_pern <- rbind(delegates_april_2019,
                                      Villia_maria_april_2019,
                                      pernod_ricard_april_2019)
 write_csv(delegate_wither_villia_pern, "delegate_wither_villia_pern.csv")
+write_csv(delegate_wither_villia_pern, "delegate_wither_villia_pern1.csv")
+ 
