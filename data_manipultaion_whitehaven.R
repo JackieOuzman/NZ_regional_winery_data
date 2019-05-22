7c6201b58576f4c93d54738ffb8b00a0e651c621
Jackie Ouzman <jackie.ouzman@csiro.au>
2019-05-22 14:22:09 +0930

    New data set whitehaven

---
+++ data_manipultaion_whitehaven.R
@@ -0,0 +1,8 @@
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+library(readxl)
+
+white_haven <- read_excel("V:/Marlborough regional/Regional winery data/Raw_data/Harvest  Mapping Data Whitehaven.xlsx", 
+                             sheet = "All - Data")
