68e50f000a7556f43be930d1c7fc9c8ba765f2be
Jackie Ouzman <jackie.ouzman@csiro.au>
2019-05-31 12:14:17 +0930

    looking into problem with block id code not matching graphing outputs

--- data_manipultaion_wither_hills.R
+++ data_manipultaion_wither_hills.R
@@ -19,7 +19,8 @@ wither_hills_GPS <- wither_hills_GPS_temp %>%
                     select(ID_temp = Name ,
                     x_coord = POINT_X,
                     y_coord = POINT_Y) %>% 
-                    mutate(ID_temp = gsub( "-", "_", ID_temp))
+                    mutate(ID_temp = tolower(gsub( "-", "_", ID_temp)))
+
 glimpse(wither_hills_GPS)
 #####################################################################################################################
 ##############################           Add in the block info  #########################################################
@@ -34,8 +35,14 @@ wither_hills_block_info <- wither_hills_block_info %>%
                             ID_temp = X__1, 
                             variety,
                             row_spacing = `row spacing (m)`) %>% 
-                            mutate(ID_temp = gsub( "-", "_", ID_temp))
+                            mutate(ID_temp = tolower(gsub( "-", "_", ID_temp)))
 glimpse(wither_hills_block_info)
+###there is a data entry error om__0.3a_sub find and replace with om_03a_sb
+
+wither_hills_block_info <- mutate(wither_hills_block_info,
+                                  ID_temp =  case_when(
+                                    ID_temp == "om__0.3a_sb" ~ "om_03a_sb",
+                               TRUE ~ ID_temp))
 ###Join GPS and block data
 
 glimpse(wither_hills_GPS) #85
@@ -43,11 +50,11 @@ glimpse(wither_hills_block_info) #83
 
 wither_hills_GPS_block_info <- full_join(wither_hills_GPS,wither_hills_block_info, by = "ID_temp")
 
-glimpse(wither_hills_GPS_block_info) #88
+glimpse(wither_hills_GPS_block_info) #86
 #drop the winery data entery
 wither_hills_GPS_block_info <- filter(wither_hills_GPS_block_info, ID_temp != "winery")
 
-glimpse(wither_hills_GPS_block_info) #88
+glimpse(wither_hills_GPS_block_info) #85
 
 #####################################################################################################################
 ##############################           Add in the yield measures  #########################################################
@@ -56,6 +63,15 @@ glimpse(wither_hills_GPS_block_info) #88
 wither_hills_harvest_details <- read_excel("V:/Marlborough regional/Regional winery data/Raw_data/Wither Hills yield data For Mike Trought RGVB.xlsx", 
                                     sheet = "15,16,17,18 ", skip = 3)
 glimpse(wither_hills_harvest_details)
+#bm_02_(spur09)_pn is only block name on the harvest data, and GPS point only have bm_02_pn – make the assumption that they are the same?
+wither_hills_harvest_details <- mutate(wither_hills_harvest_details,
+                                       Block =  case_when(
+                                         Block == "BM 02 (spur 09)" ~ "BM 02",
+                                         TRUE ~ Block))
+
+
+
+
 #Create an ID clm
 wither_hills_harvest_details_step1 <- wither_hills_harvest_details %>% 
          mutate(vineyard_lower =str_to_lower(Vineyard, locale = "en"),
@@ -115,6 +131,7 @@ wither_hills_harvest_details <- wither_hills_harvest_details %>%
          berry_wt = bunch_mass_g / berry_bunch,
          julian = as.numeric(format(harvest_date, "%j")))
 
+
 glimpse(wither_hills_harvest_details)
 
 #####################################################################################################################
@@ -123,12 +140,12 @@ glimpse(wither_hills_harvest_details)
 
 
 #### join the GPS files to the harvest data files
-glimpse(wither_hills_GPS_block_info) #87
+glimpse(wither_hills_GPS_block_info) #85
 glimpse(wither_hills_harvest_details) #583
 wither_hills_GPS_block_info_harvest <- full_join(wither_hills_GPS_block_info, wither_hills_harvest_details,
                                                  by= "ID_temp") %>% 
   mutate(company = "Wither_Hills")
-glimpse(wither_hills_GPS_block_info_harvest)
+glimpse(wither_hills_GPS_block_info_harvest) #606
 
 wither_hills_GPS_block_info_harvest <- wither_hills_GPS_block_info_harvest %>% 
   select(company, ID = ID_temp, variety, x_coord, y_coord,
@@ -142,7 +159,7 @@ glimpse(wither_hills_GPS_block_info_harvest)
 
 
 #the date is not set as such need to be fixed up
-library(lubridate)
+
 glimpse(wither_hills_GPS_block_info_harvest$year)
 
 wither_hills_GPS_block_info_harvest$year <- as.double(wither_hills_GPS_block_info_harvest$year)
@@ -160,34 +177,47 @@ glimpse(wither_hills_GPS_block_info_harvest)
 dim(wither_hills_GPS_block_info_harvest)
 #how many site?
 dim(wither_hills_GPS_block_info_harvest)
-glimpse(wither_hills_GPS_block_info_harvest$year) #610 records
+glimpse(wither_hills_GPS_block_info_harvest$year) #606 records
 
-max(wither_hills_GPS_block_info_harvest$year) #2008 -2018
-min(wither_hills_GPS_block_info_harvest$year) #2008 -2018
+max(wither_hills_GPS_block_info_harvest$year, na.rm = TRUE) #2015 -2018
+min(wither_hills_GPS_block_info_harvest$year, na.rm = TRUE) #2015 -2018
 
 
 #how many sites with GPS pts
-glimpse(wither_hills_GPS_block_info_harvest)#610 records
-#how many sites with GPS pts by Variety
+glimpse(wither_hills_GPS_block_info_harvest)#606 records
+colSums(is.na(wither_hills_GPS_block_info_harvest)) #330 with missing GPS records
+GPS_only <- filter(wither_hills_GPS_block_info_harvest, x_coord> 0)
+glimpse(GPS_only)
+
+
+
+#how many sites with GPS pts by Variety using filter
 filter(wither_hills_GPS_block_info_harvest, x_coord > 0) %>% 
-ggplot( aes(variety))+
+  ggplot( aes(variety))+
   geom_bar()+
   theme_bw()+
   theme(axis.text.x=element_text(angle=90))+
   labs(y = "Count of sites with GPS coordinates")
-GPS_only <- filter(wither_hills_GPS_block_info_harvest, x_coord> 0)
-glimpse(GPS_only)
+#how many sites with GPS pts by Variety using df with just GPS pts (same graph different methods)
+ggplot(GPS_only, aes(variety))+
+  geom_bar()+
+  theme_bw()+
+  theme(axis.text.x=element_text(angle=90))+
+  labs(y = "Count of sites with GPS coordinates")
 
-#how many sites by Variety by year
-ggplot(pernod_ricard1, aes(variety))+
+
+
+
+#how many sites by Variety by year with just the GPS data
+ggplot(wither_hills_GPS_block_info_harvest, aes(variety))+
   geom_bar()+
   theme_bw()+
   theme(axis.text.x=element_text(angle=90))+
   labs(y = "Count of sites")+
   facet_wrap(~year)
 
-#how many sites by Variety
-ggplot(pernod_ricard1, aes(variety))+
+#how many sites by Variety with just the GPS data all the data
+ggplot(wither_hills_GPS_block_info_harvest, aes(variety))+
   geom_bar()+
   theme_bw()+
   theme(axis.text.x=element_text(angle=90))+
@@ -196,23 +226,23 @@ ggplot(pernod_ricard1, aes(variety))+
 
 
 #create a new variable year_as_factor
-pernod_ricard1$year_factor <- as.factor(pernod_ricard1$year)
-
+wither_hills_GPS_block_info_harvest$year_factor <- as.factor(wither_hills_GPS_block_info_harvest$year)
+glimpse(wither_hills_GPS_block_info_harvest)
 #filter data for Sauvignon Blanc
-pernod_ricard1_sau <- filter(pernod_ricard1, variety == "Sauvignon Blanc") 
-glimpse(pernod_ricard1_sau)
+wither_hills_GPS_block_info_harvest_sau <- filter(wither_hills_GPS_block_info_harvest, variety == "sb") 
+glimpse(wither_hills_GPS_block_info_harvest_sau)
 
 #how many sites for Sauvignon Blanc by year
-group_by(pernod_ricard1_sau, year) %>% 
+group_by(wither_hills_GPS_block_info_harvest_sau, year) %>% 
   count()
 #how many sites for Sauvignon Blanc have missing data - how much missing data?
-ggplot(pernod_ricard1_sau, aes(year_factor, na_count))+
+ggplot(wither_hills_GPS_block_info_harvest_sau, aes(year_factor, na_count))+
   geom_col()+
   theme_bw()+
   labs(x = "Year",
        y= "Total counts of missing data entries NA - Sauvignon Blanc")
 #how many sites for Sauvignon Blanc have missing data - missing data grouped together?
-ggplot(pernod_ricard1_sau, aes(na_count))+
+ggplot(wither_hills_GPS_block_info_harvest_sau, aes(na_count))+
   geom_bar()+
   scale_x_continuous(breaks =  c(2,4,6,8,10))+
   facet_wrap(~year_factor)+
@@ -221,23 +251,23 @@ ggplot(pernod_ricard1_sau, aes(na_count))+
        y= "Counts of missing data entries NA")
 
 
-glimpse(pernod_ricard1_sau)
+glimpse(wither_hills_GPS_block_info_harvest_sau)
 #julian days
-ggplot(pernod_ricard1_sau, aes(year_factor, julian))+
+ggplot(wither_hills_GPS_block_info_harvest_sau, aes(year_factor, julian))+
   geom_boxplot(alpha=0.1)+
   geom_point(colour = "blue", alpha = 0.1)+
   theme_bw()+
   labs(x = "Year",
        y= "Julian days - Sauvignon Blanc")
 #yield_t_ha
-ggplot(pernod_ricard1_sau, aes(year_factor, yield_t_ha))+
+ggplot(wither_hills_GPS_block_info_harvest_sau, aes(year_factor, yield_t_ha))+
   geom_boxplot(alpha=0.1)+
   geom_point(colour = "blue", alpha = 0.1)+
   theme_bw()+
   labs(x = "Year",
        y= "Yield t/ha - Sauvignon Blanc")
 #yield_kg_m
-ggplot(pernod_ricard1_sau, aes(year_factor, yield_kg_m))+
+ggplot(wither_hills_GPS_block_info_harvest_sau, aes(year_factor, yield_kg_m))+
   geom_boxplot(alpha=0.1)+
   geom_point(colour = "blue", alpha = 0.1)+
   theme_bw()+
@@ -245,7 +275,7 @@ ggplot(pernod_ricard1_sau, aes(year_factor, yield_kg_m))+
        y= "yield kg/m - Sauvignon Blanc")
 
 #yield_kg_m filter out zeros
-filter(pernod_ricard1_sau,yield_kg_m != 0) %>% 
+filter(wither_hills_GPS_block_info_harvest_sau,yield_kg_m != 0) %>% 
   ggplot( aes(year_factor, yield_kg_m))+
   geom_boxplot(alpha=0.1)+
   geom_point(colour = "blue", alpha = 0.1)+
@@ -255,7 +285,7 @@ filter(pernod_ricard1_sau,yield_kg_m != 0) %>%
 
 
 #brix - too many zero
-ggplot(pernod_ricard1_sau, aes(year_factor, brix))+
+ggplot(wither_hills_GPS_block_info_harvest_sau, aes(year_factor, brix))+
   geom_boxplot(alpha=0.1)+
   geom_point(colour = "blue", alpha = 0.1)+
   theme_bw()+
@@ -264,7 +294,7 @@ ggplot(pernod_ricard1_sau, aes(year_factor, brix))+
 
 
 #brix - filter out zero
-filter(pernod_ricard1_sau,brix != 0) %>% 
+filter(wither_hills_GPS_block_info_harvest_sau,brix != 0) %>% 
   ggplot( aes(year_factor, brix))+
   geom_boxplot(alpha=0.1)+
   geom_point(colour = "blue", alpha = 0.1)+
@@ -275,9 +305,9 @@ filter(pernod_ricard1_sau,brix != 0) %>%
 
 ############################################################################## 
 ########################    File to use   ####################################
-pernod_ricard1_sau <- select(pernod_ricard1_sau, -year_factor)
-glimpse(pernod_ricard1_sau)
-write_csv(pernod_ricard1_sau, "V:/Marlborough regional/working_jaxs/pernod_ricard1_sau.csv")
+wither_hills_GPS_block_info_harvest_sau <- select(wither_hills_GPS_block_info_harvest_sau, -year_factor)
+glimpse(wither_hills_GPS_block_info_harvest_sau)
+write_csv(pernod_ricard1_sau, "V:/Marlborough regional/working_jaxs/wither_hills_GPS_block_info_harvest_sau.csv")
 ##############################################################################   
 
 
