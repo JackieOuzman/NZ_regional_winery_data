78b3b1cb2aab2e71fddafa31a976cd7e9967fae6
Jackie Ouzman <jackie.ouzman@csiro.au>
2020-08-05 16:32:58 +0930

    working my waqy through the changes

--- data_manipultaion_babich.R
+++ data_manipultaion_babich.R
@@ -9,6 +9,97 @@ library(data.table)
 library(stringr)
 library(tidyr)
 
+####################################################################################################################
+######   Bring in the coodinated that I have got from the pdf maps and street address     ##########################
+
+
+
+babich_coordinates <- read_excel("V:/Marlborough regional/Regional winery data/Raw_data/Babich/google_earth_location/Babich_locations_with_v_spacing_r_width CSIRO Amended 050820.xlsx",
+                                sheet =  "Babich_locations_with_v_spacing")
+
+names(babich_coordinates)
+
+babich_coordinates <- filter(babich_coordinates,
+                             !is.na(POINT_X...2))
+
+
+
+babich_coordinates <- select(babich_coordinates,
+                             "ID",
+                             "POINT_X...2",
+                             "POINT_Y...3",
+                             "row_spacing",
+                             "vine_spacing",
+                             "block" ,
+                             "Variety" ,
+                            "vineyard code" )
+
+
+babich_coordinates <- rename(babich_coordinates, 
+                             "POINT_X" = "POINT_X...2",
+                             "POINT_Y" ="POINT_Y...3",
+                             "variety" ="Variety" ,
+                             "vineyard_code" = "vineyard code" )
+
+
+### change the coordinates to from long and lats to projected data.
+# I have my data in decimal degrees I want to convert it into GDA
+mapCRS <- CRS("+init=epsg:2193")     # 2193 = NZGD2000 / New Zealand Transverse Mercator 2000 
+wgs84CRS <- CRS("+init=epsg:4326")   # 4326 WGS 84 - assumed for input lats and longs
+
+names(babich_coordinates)
+coordinates(babich_coordinates) <- ~POINT_X+POINT_Y
+proj4string(babich_coordinates) <- wgs84CRS   # assume input lat and longs are WGS84
+babich_coordinates1 <- spTransform(babich_coordinates, mapCRS)
+
+glimpse(babich_coordinates1)
+babich_coordinates1_df = as.data.frame(babich_coordinates1) #this has the new coordinates projected !YES!!
+glimpse(babich_coordinates1_df)
+
+rm(Babich_2015, Babich_2015_SB, babich_coordinates, babich_coordinates1, mapCRS, wgs84CRS)
+
+############################################################################################################
+### Bring in the yield data for multiple years        ######################################################
+
+Babich_2014_17 <- read_excel("V:/Marlborough regional/Regional winery data/Raw_data/Babich/revised_data_05082020/Historical Harvest Details V14-V17 CSIRO 050820.xlsx",
+                             skip = 2)
+                             
+names(Babich_2014_17)
+
+Babich_2014_17 <- fill(Babich_2014_17, "Corporate Blocks", .direction = "down")
+
+Babich_2014_17 <- rename(
+  Babich_2014_17,
+  "vineyard" = "Corporate Blocks",
+  "notes" = "...2"   ,
+  "blocks" =  "...4"  ,
+  "ha" =  "...5"  ,
+  "harvest_date_14" = "vintage 14"  ,
+  "tonnes_14" = "...7",
+  "brix_14" = "...8" ,
+  "harvest_date_15" = "vintage 15"  ,
+  "tonnes_15" = "...10" ,
+  "brix_15" = "...11",
+  "harvest_date_16" = "vintage 16",
+  "tonnes_16" = "...13" ,
+  "brix_16" = "...14" ,
+  "harvest_date_17" = "vintage 17"   ,
+  "tonnes_17" = "...16"  ,
+  "brix_17" = "...17"
+)
+
+
+Babich_2014_17 <- filter(Babich_2014_17,
+                             !is.na(blocks))
+
+
+#### up to here #####
+#####################################################################################################################
+
+
+
+
+
 Babich_2015 <- read_excel("V:/Marlborough regional/Regional winery data/Raw_data/Babich/2015 Vintage counts.xlsx", 
                                    sheet = "actual weights", skip = 1)
 names(Babich_2015)
