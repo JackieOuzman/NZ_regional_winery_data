1e51682fd1ecb34f551de90b344531f10b7b1b23
Jackie Ouzman <jackie.ouzman@csiro.au>
2020-08-10 17:07:10 +0930

    no year in some rows? why?? need to change coodinates to projected.

--- data_manipultaion_Grower coop .R
+++ data_manipultaion_Grower coop .R
@@ -54,12 +54,11 @@ Grower_coop_V2019_GPS <- read_excel("V:/Marlborough regional/Regional winery dat
 names(Grower_coop_V2019_GPS) #Realignment Number
 names(Grower_coop_V2019_block_info)
 
-Grower_coop_V2019_GPS <- rename(Grower_coop_V2019_GPS,
-                                "Realignment Number" = "Site_numb")
-
 #now I can join them....
-names(Grower_coop_V2019_block_info) 
-names(Grower_coop_V2019_GPS)
+str(Grower_coop_V2019_block_info) 
+str(Grower_coop_V2019_GPS)
+Grower_coop_V2019_GPS$`Realignment Number`<- as.numeric(Grower_coop_V2019_GPS$`Realignment Number`)
+
 Grower_coop_V2019 <- full_join(Grower_coop_V2019_block_info, Grower_coop_V2019_GPS, by= "Realignment Number")
 #whats missing?
 
@@ -182,7 +181,8 @@ Grower_coop_V2019 <- select(Grower_coop_V2019_join,
                                              #pruning_style,
                                              row_width ,
                                              vine_spacing = vine_Spacing,
-                                             Realignment_number
+                                             Realignment_number,
+                                             bunch_weight
 )
 
 
@@ -197,6 +197,224 @@ rm(#Grower_coop_V2019
    Grower_coop_V2019_selection, 
    Grower_coop_V2019_yld_data)
   
+##############################################################################################################################
+############ 2014-2018 data ######################################################################################################
+
+
+Harvest_data_2014_2017 <- read_excel("V:/Marlborough regional/Regional winery data/Raw_data/Marlborough_Grape_Growers_Cooperative/Harvest data 2014-2017.xlsx", 
+                                     sheet = "2014_2017_codes")
+
+names(Harvest_data_2014_2017)
+names(Grower_coop_V2019)
+
+#filter out data so I am keeping sites that have a good match to the 2019 data. clm 
+
+Harvest_data_2014_2017 <- filter(Harvest_data_2014_2017,
+                                 match_of_Realignment_and_ha == "y")
+
+
+#Divide up per year to make data long
+Harvest_data_2014 <- dplyr::select(
+   Harvest_data_2014_2017,
+   "Realignment Number"  = "Realignment Number best match",
+   "Grower" ,
+   ha = "Hectares",
+   yld = "Fruit Produced 2014",
+   yield_t_ha = "Tonnes per hectare 2014" ,
+   brix = "2014 Brix"
+) %>%
+   mutate(year = 2014)
+
+Harvest_data_2015 <- dplyr::select(
+   Harvest_data_2014_2017,
+   "Realignment Number"  = "Realignment Number best match",
+   "Grower" ,
+   ha = "Hectares",
+   yld = "Fruit Produced 2015",
+   yield_t_ha = "Tonnes per hectare 2015" ,
+   brix = "2015 Brix"
+) %>%
+   mutate(year = 2015)
+
+Harvest_data_2016 <- dplyr::select(
+   Harvest_data_2014_2017,
+   "Realignment Number"  = "Realignment Number best match",
+   "Grower" ,
+   ha = "Hectares",
+   yld = "Fruit Produced 2016",
+   yield_t_ha = "Tonnes per hectare 2016" ,
+   brix = "2016 Brix"
+) %>%
+   mutate(year = 2016)
+
+Harvest_data_2017 <- dplyr::select(
+   Harvest_data_2014_2017,
+   "Realignment Number"  = "Realignment Number best match",
+   "Grower" ,
+   ha = "Hectares",
+   yld = "Fruit Produced 2017",
+   yield_t_ha = "Tonnes per hectare 2017" ,
+   brix = "2017 Brix"
+) %>%
+   mutate(year = 2017)
+
+#join them all togther again..
+ yld_data2014_2017 <- bind_rows(Harvest_data_2014,
+                                Harvest_data_2015,
+                                Harvest_data_2016,
+                                Harvest_data_2017)
+ rm("Harvest_data_2014",
+    "Harvest_data_2015",
+    "Harvest_data_2016",
+    "Harvest_data_2017")
+ 
+yld_data2014_2017$yield_t_ha <- replace(yld_data2014_2017$yield_t_ha, 
+                                      yld_data2014_2017$yield_t_ha == 0, NA)
+yld_data2014_2017$yld <- replace(yld_data2014_2017$yld, 
+                                      yld_data2014_2017$yld == 0, NA)
+
+#check to see if the t/ha and ha are a good - match some sites might have an error but
+# yield in t/ha looks sensible - so keep it?
+ yld_data2014_2017 <- mutate(yld_data2014_2017,
+                            check_t_ha = yld /ha)
+str(yld_data2014_2017)
+yld_data2014_2017 <- mutate(yld_data2014_2017,
+                            match_ha = case_when(
+                               check_t_ha == yield_t_ha  ~ 1,
+                               TRUE ~ 0
+                            ))
+names(yld_data2014_2017)
+site_with_t_ha_error <- filter(yld_data2014_2017, !is.na(yld))
+site_with_t_ha_error <- filter(site_with_t_ha_error,
+                               match_ha == 0)
+
+
+
+### add in the coodinates based on relaingnmnet numbers
+str(yld_data2014_2017)
+str(Grower_coop_V2019_GPS)
+
+yld_data2014_2017$`Realignment Number`<- as.numeric(yld_data2014_2017$`Realignment Number`)
+
+yld_data2014_2017 <- full_join(yld_data2014_2017,Grower_coop_V2019_GPS )
+yld_data2014_2017 <- dplyr::select(yld_data2014_2017,
+                                   - "...6",
+                                   -"notes",
+                                   -"check_t_ha" ,
+                                  - "match_ha" )
+
+#I need to add in the row spacing and vine spacing this is in the 2019 data
+names(Grower_coop_V2019)
+row_vine_spacing <- dplyr::select(Grower_coop_V2019,
+                                  "Realignment_number" ,
+                                  row_width  ,
+                                  vine_spacing)
+
+# add this to the other 2014 -2017 data
+str(row_vine_spacing)
+str(yld_data2014_2017)
+#rename clm 
+yld_data2014_2017 <- rename(yld_data2014_2017,
+                            Realignment_number = `Realignment Number`)
+yld_data2014_2017$Realignment_number <- as.numeric(yld_data2014_2017$Realignment_number)
+yld_data2014_2017 <- left_join(yld_data2014_2017, row_vine_spacing )
+
+
+#### Add in the calulations
+yld_data2014_2017 <- mutate(yld_data2014_2017,
+                                 harvest_date= NA,
+                                 julian = NA, #julian = as.numeric(format(harvest_date, "%j")),
+                                 m_ha_vine = 10000/ row_width,
+                                 yield_kg_m = (yield_t_ha *1000)/m_ha_vine,
+                                 bunch_weight = NA, 
+                                 berry_per_bunch = NA ,
+                                 bunches_per_vine = NA, #??not sure I can cal this?? help
+                                 pruning_style = NA,
+                                 brix ,
+                                 meter_row_per_ha = 10000/row_width,
+                                 yld_per_m_row_kg = (yield_t_ha *1000) / 10000/row_width,
+                                 bunch_m = NA)
+
+
+str(yld_data2014_2017)
+str(Grower_coop_V2019_GPS)
+Grower_coop_V2019_GPS <- rename(Grower_coop_V2019_GPS,
+                            Realignment_number = `Realignment Number`)
+
+
+yld_data2014_2017 <- mutate(yld_data2014_2017,
+                            company = "grower_coop",
+                            ID_yr = paste0("Realignment_numb_", Realignment_number, "_year_", year ),
+                            variety = "SAB")
+
+yld_data2014_2017 <- select(
+   yld_data2014_2017,
+   company,
+   ID_yr,
+   year, 
+   variety,
+   x_coord = POINT_X,
+   y_coord = POINT_Y ,
+   year,
+   harvest_date,
+   julian,
+   bunch_weight,
+   yield_t_ha,
+   yield_kg_m,
+   brix, 
+   bunch_m,
+   pruning_style,
+   row_width ,
+   vine_spacing,
+   Block = Name 
+)
+
+names(yld_data2014_2017)
+
+rm("row_vine_spacing",
+   "site_with_t_ha_error")
+rm("Grower_coop_V2019_GPS")
+
+# join the 2019 and 2014-2018 data togther
+
+str(yld_data2014_2017)
+str(Grower_coop_V2019) #add in a fw more clms
+Grower_coop_V2019 <- mutate(Grower_coop_V2019,
+                            company = "grower_coop",
+                            ID_yr = paste0("Realignment_numb_", Realignment_number, "_year_", year ),
+                            variety = "SAB",
+                            pruning_style = NA,
+                            )
+
+
+Grower_coop_V2019 <- select(
+   Grower_coop_V2019,
+   company,
+   ID_yr,
+   year, 
+   variety,
+   x_coord ,
+   y_coord  ,
+   year,
+   harvest_date,
+   julian,
+   #bunch_weight,
+   yield_t_ha,
+   yield_kg_m,
+   brix, 
+   bunch_m,
+   #pruning_style,
+   row_width ,
+   vine_spacing
+   
+)
+
+
+V2019_2014_to_2017 <- bind_rows(Grower_coop_V2019, yld_data2014_2017)
+
+write.csv( V2019_2014_to_2017, "grower_coop_V2019_2014_to_2017.csv")
+
+#### STOP HERE I NEED HELP with the 2018 data
 
 
 ##############################################################################################################################
@@ -272,156 +490,3 @@ rm(Grower_coop_V2018_part1, Grower_coop_V2018_part1_av, Grower_coop_V2018_part2,
 
 # but I am having huge problems joining this data
 
-##############################################################################################################################
-############ 2014 - 2017  data ######################################################################################################
-
-
-Harvest_data_2014_2017 <- read_excel("V:/Marlborough regional/Regional winery data/Raw_data/Marlborough_Grape_Growers_Cooperative/Harvest data 2014-2017.xlsx", 
-                                     sheet = "2014_2017_codes")
-
-str(Harvest_data_2014_2017)
-Harvest_data_2014_2017 <- filter(Harvest_data_2014_2017,Grower != "NA" )
-Harvest_data_2014_2017 <- filter(Harvest_data_2014_2017,Hectares != "NA" )
-
-#keep the data that has a good match with the name and ha.
-Harvest_data_2014_2017 <- filter(Harvest_data_2014_2017, match_of_Realignment_and_ha != "n")
-
-#split the data into years
-Harvest_data_2014 <- dplyr::select(Harvest_data_2014_2017,
-                                   Grower ,
-                                   Realignment_Number = "Realignment Number best match", 
-                                   ha = "Hectares",
-                                   yld = "Fruit Produced 2014" ,
-                                   yield_t_ha = "Tonnes per hectare 2014",
-                                   brix = "2014 Brix") %>% 
-  mutate(year = 2014)
-
-
-Harvest_data_2015 <- dplyr::select(Harvest_data_2014_2017,
-                                   Grower ,
-                                   Realignment_Number = "Realignment Number best match", 
-                                   ha = "Hectares",
-                                   yld = "Fruit Produced 2015" ,
-                                   yield_t_ha = "Tonnes per hectare 2015",
-                                   brix = "2015 Brix") %>% 
-  mutate(year = 2015)
-
-Harvest_data_2016 <- dplyr::select(Harvest_data_2014_2017,
-                                   Grower ,
-                                   Realignment_Number = "Realignment Number best match", 
-                                   ha = "Hectares",
-                                   yld = "Fruit Produced 2016" ,
-                                   yield_t_ha = "Tonnes per hectare 2016",
-                                   brix = "2016 Brix") %>% 
-  mutate(year = 2016)
-
-Harvest_data_2017 <- dplyr::select(Harvest_data_2014_2017,
-                                   Grower ,
-                                   Realignment_Number = "Realignment Number best match", 
-                                   ha = "Hectares",
-                                   yld = "Fruit Produced 2017" ,
-                                   yield_t_ha = "Tonnes per hectare 2017",
-                                   brix = "2017 Brix") %>% 
-  mutate(year = 2017)
-
-str(Harvest_data_2014)
-str(Harvest_data_2015)
-str(Harvest_data_2016)
-str(Harvest_data_2017)
-Harvest_data_2017$yld <- as.numeric(Harvest_data_2017$yld)
-Harvest_data_2017$yield_t_ha <- as.numeric(Harvest_data_2017$yield_t_ha)
-
-
-
-
-Harvest_data_2014_2017_tidy <- bind_rows(Harvest_data_2014,
-                                         Harvest_data_2015,
-                                         Harvest_data_2016,
-                                         Harvest_data_2017)
-
-## what the go with the names are there any replicates? _ nope looks ok
-growers2014 <-unique(Harvest_data_2014$Grower)
-growers2015 <-unique(Harvest_data_2015$Grower)
-growers2016 <-unique(Harvest_data_2016$Grower)
-growers2017 <-unique(Harvest_data_2017$Grower)
-
-
-
-###############################################################################################
-##### 2019 GPS sites - done via google earth and maps ########################################
-str(Harvest_data_2014_2017_tidy)
-Harvest_data_2014_2017_tidy$Realignment_Number <- as.numeric(Harvest_data_2014_2017_tidy$Realignment_Number)
-str(Grower_coop_V2019)
-Grower_coop_V2019_selection <- dplyr::select(Grower_coop_V2019,
-                                             #ID_yr,
-                                             #SWNZ_Vineyard_ID = "SWNZ Vineyard ID",
-                                             row_width ,
-                                             vine_spacing ,
-                                             "Realignment_Number" = "Realignment_number",
-                                             x_coord,
-                                             y_coord
-)
-
-
-str(Harvest_data_2014_2017_tidy)
-str(Grower_coop_V2019_selection)
-
-Harvest_data_2014_2017_GPS <- left_join(Harvest_data_2014_2017_tidy,Grower_coop_V2019_selection )
-
-rm(Harvest_data_2014_2017_tidy, Grower_coop_V2019_GPS,
-   Harvest_data_2014, Harvest_data_2015, Harvest_data_2016, Harvest_data_2017)
-
-
-#### Add in the other fields
-str(Harvest_data_2014_2017_GPS)
-#### Add in the calulations
-Harvest_data_2014_2017_GPS <- mutate(Harvest_data_2014_2017_GPS,
-                                 ID_yr = paste0( Grower,"_", year),
-                                 harvest_date= "NA",
-                                 bunch_m = "NA",
-                                 company = "Grower_coop",
-                                 julian = NA, #julian = as.numeric(format(harvest_date, "%j")),
-                                 m_ha_vine = 10000/ row_width,
-                                 yield_kg_m = (yield_t_ha *1000)/m_ha_vine,
-                                 #bunch_weight = total_weight_bunches / nunb_bunches_collected, #the data sheet has -4 from this cal ?why
-                                 #berry_weight_1 = total_weight_berry / no_berry_in_sample,
-                                 #berry_per_bunch = bunch_weight / berry_weight_1, 
-                                 #berry_weight_2 = (yield_t_ha/vines_ha/bunches_per_vine/berry_per_bunch)*1000000, #this is in the data sheet - check
-                                 #bunches_per_vine = NA, #??not sure I can cal this?? help
-                                 pruning_style = NA,
-                                 brix ,
-                                 meter_row_per_ha = 10000/row_width,
-                                 yld_per_m_row_kg = (yield_t_ha *1000) / 10000/row_width)#,
-                                 #bunch_m = (yld_per_m_row_kg * 1000)/ bunch_weight)
-
-
-
-str(Harvest_data_2014_2017_GPS)
-Harvest_data_2014_2017_GPS <- select(Harvest_data_2014_2017_GPS,
-                            company ,
-                            ID_yr , 
-                            #variety,
-                            x_coord ,
-                            y_coord  ,
-                            year,
-                            harvest_date, #missing
-                            julian, #missing
-                            yield_t_ha,
-                            yield_kg_m,
-                            brix, #missing
-                            bunch_m ,
-                            #pruning_style,
-                            row_width ,
-                            vine_spacing,
-                            Realignment_Number
-)
-###### Now join to the 2019 data 
-names(Grower_coop_V2019)
-names(Harvest_data_2014_2017_GPS)
-Grower_coop_V2019 <- rename(Grower_coop_V2019,
-                            "Realignment_Number" = "Realignment_number")
-str(Grower_coop_V2019)
-str(Harvest_data_2014_2017_GPS)
-
-Grower_coop_V2014_2019ex2018 <- rbind(Grower_coop_V2019, Harvest_data_2014_2017_GPS)
-View(Grower_coop_V2014_2019ex2018)
