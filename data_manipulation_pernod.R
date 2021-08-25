7d112e54639c77512f3bb7f034796e5bd8c32b0a
Jackie Ouzman <jackie.ouzman@csiro.au>
2021-08-25 11:51:01 +0930

    not sure what happened

--- data_manipulation_pernod.R
+++ data_manipulation_pernod.R
@@ -97,11 +97,53 @@ perno_GPS_distinct <- mutate(perno_GPS_distinct,
                                ID == "M16_SBLI" ~ 5406747.31, 
                                TRUE ~ y_coord))
 
-
+###need to check all of below code and make sure it works
  
+perno_GPS_distinct <- perno_GPS_distinct %>% 
+  select(ID, year, x_coord, y_coord, Variety, Vnd)
+glimpse(perno_GPS_distinct)
 
 write_csv(perno_GPS_distinct, path = "V:/Marlborough regional/working_jaxs/perno_GPS_test1.csv" )
 
 
-#This is working now all the coordinates are in the corrcet spot
-                          
+#Bring in the yld data and add to coordiates 
+#perno_yld <- read_excel("V:/Marlborough regional/Regional winery data/Raw_data/Pernod Ricard Trought BX BchWT 2008 -2018 Co Marl.xlsx", 
+#                             sheet = "Block Yield reference") 
+perno_yld <- read_excel("C:/Users/ouz001/NZ_work/test/Pernod Ricard Trought BX BchWT 2008 -2018 Co Marl.xlsx" ,
+                            sheet = "Block Yield reference")
+
+perno_yld <- select(perno_yld,
+                    ID_temp = `VendorBlock Key` ,
+                    Vnd = `Vendor Code`,
+                    Block = `Block Ref Number`,
+                    Yr = `Vintage`,
+                    brix = `Brix at Harvest`,
+                    yield_kg_per_m = `Harvested KG per M`) %>% 
+  mutate(ID = paste0(Vnd,"_", Block),
+         year = as.double(paste0(Yr+2000)))
+#tidy this up
+perno_yld <- perno_yld %>% 
+  select(ID, yield_kg_per_m, brix)
+
+###Join
+test_join <- left_join(perno_yld, perno_GPS_distinct, by =  "ID")
+
+#what was not joined and what variety was it?
+test_anti_join <- anti_join(perno_yld, perno_GPS_distinct, by =  "ID")
+#for what was not joined add the variety 264 - includes all the years
+test_anti_join <- test_anti_join %>% 
+  separate( ID, into = c("vendor", "variety"), sep = "_", remove = FALSE )
+#just get the unquie blocks 123 blocks
+test_anti_join <- distinct(test_anti_join, ID)
+test_anti_join <- test_anti_join %>% 
+  separate( ID, into = c("vendor", "variety"), sep = "_", remove = FALSE )
+#how many of these block that didnt get coord are SBLB blocks?
+test_anti_join <- distinct(test_anti_join, ID, .keep_all = TRUE)
+glimpse(test_anti_join)  
+test_anti_join_variety <- test_anti_join %>% 
+  group_by(variety) %>% 
+  summarise(test_anti_join =n())
+
+
+test_join <- test_join %>% 
+  filter(x_coord > 0)
