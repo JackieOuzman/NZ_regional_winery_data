63d19965339ae3470bf7bdc65d338900bfe870f3
Jackie Ouzman <jackie.ouzman@csiro.au>
2022-05-30 11:58:23 +0930

    suss out a problem with data frame miss match

--- clustering/step3_cluster_prep_vineyard_climate.R
+++ clustering/step3_cluster_prep_vineyard_climate.R
@@ -3,3 +3,19 @@
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
+
+
+vineyard <- read.csv("V:/Marlborough regional/working_jaxs/for_mapping_july2022/vineyard_all_cluster_input.csv")
+climate <- read.csv("V:/Marlborough regional/climate/climate_data_2022_vineyards_R/Climate_data_as_pts/climate_all_cluster_input.csv")
+
+
+names(vineyard)
+names(climate)
+
+# I was expecting the files to have the same size, but I was also expecting that the climate might be less than vinyards
+# but this is the opposite the vineyard file is smaller ? why
+# they use the same grid file to extrcat the pts.
+grid_pts_df <- read.csv("V:/Marlborough regional/Council/Regional_Grid/Marl_Ag_200_BlockGrid_100m_pts.csv") #used for climate
+grid_pts_df <- read.csv("V:/Marlborough regional/Council/Regional_Grid/Marl_Ag_200_BlockGrid_100m_pts.csv") #used for vineyards / winery
+
+#Join on field called for_join
